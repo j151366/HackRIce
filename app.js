a60a127028b0a3e5651c76608cfc541e941afbf5
李海瑞 <max179303@gmail.com>
2021-09-17 23:17:03 -0500

    add firebase store

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const http = require('http');
+const express = require('express');
 const admin = require('firebase-admin');
-var serviceAccount = require("./hackrice-326323-firebase-adminsdk-na80w-0e642ca557.json");
+var conf = require('./conf');
+
+
+var app = express();
+
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(conf.serviceAccount)
 });
   
-
 const db = admin.firestore();
 
-
-
-
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get("/api/test", async (req, res) => {
+    const snapshot = await db.collection('test').get();
+    snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+    });
+    res.status(200).send();
+    
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+app.listen(conf.port, conf.hostname, () => {
+  console.log(`Server running at http://${conf.host}:${conf.port}/`);
 });
---
+++ conf.js
@@ -0,0 +1,7 @@
+module.exports = {
+    
+    serviceAccount : "./hackrice-326323-firebase-adminsdk-na80w-0e642ca557.json",
+    host: '127.0.0.1', // server host
+    port: 3000 // server port
+
+};
